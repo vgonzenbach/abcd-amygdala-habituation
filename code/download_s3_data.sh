3f467f6ea3bc4c6b2bc5b93d7c6754ac9649b636
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2022-09-22 13:18:53 -0400

    Add script to download data

--- code/download_s3_data.sh
+++ code/download_s3_data.sh
@@ -1,6 +1,6 @@
 
 cd $(dirname $0)/..
-bsub -J s3_download -n 50 -o logs/s3.log -e logs/s3.log python3 nda-abcd-s3-downloader/download.py \
+python3 nda-abcd-s3-downloader/download.py \
     -i datastructure_manifest.txt \
     -o bidsdata/ \
     -s subject_list.txt \
