32b58571452191f9d758fba5e8061375bc6acb42
Virgilio Gonzenbach <vjgonzenbach@gmail.com>
2023-03-13 13:43:05 -0400

    fix submodule link

--- code/download_s3_data.sh
+++ code/download_s3_data.sh
@@ -4,7 +4,7 @@ python3 nda-abcd-s3-downloader/download.py \
     -i datastructure_manifest.txt \
     -o bidsdata/ \
     -s subject_list.txt \
-    -d nda-abcd-s3-downloader/data_subsets.txt \
+    -d data_subsets.txt \
     -l logs/s3 #\
     #-c config.ini #\
     #-p 50
